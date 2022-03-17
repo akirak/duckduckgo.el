735d3ca8a96461160b6383225d3747b6135862bd
Akira Komamura <akira.komamura@gmail.com>
2022-03-18 02:25:03 +0900

    chore: Add nomake

---
+++ .nomake/flake.nix
@@ -0,0 +1,13 @@
+{
+  description =
+    "THIS IS AN AUTO-GENERATED FILE. PLEASE DON'T EDIT IT MANUALLY.";
+  inputs = {
+    duckduckgo = {
+      flake = false;
+      owner = "akirak";
+      repo = "duckduckgo.el";
+      type = "github";
+    };
+  };
+  outputs = { ... }: { };
+}
---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "FIXME: Your package description";
+
+  inputs = {
+    gnu-elpa = {
+      url = "git+https://git.savannah.gnu.org/git/emacs/elpa.git?ref=main";
+      flake = false;
+    };
+    melpa = {
+      url = "github:akirak/melpa/akirak";
+      flake = false;
+    };
+
+    nomake = {
+      url = "github:emacs-twist/nomake";
+      inputs.gnu-elpa.follows = "gnu-elpa";
+      inputs.melpa.follows = "melpa";
+    };
+  };
+
+  outputs =
+    { self
+    , nomake
+    , ...
+    } @ inputs:
+    nomake.lib.mkFlake {
+      src = ./.;
+      localPackages = [
+        "duckduckgo"
+      ];
+    };
+}
